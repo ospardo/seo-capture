7d641cfcde16a6e1ea8c76e86303a3873af4f623
Remy Prechelt <rprechelt@gmail.com>
2016-11-09 18:03:53 -0600

    Updates to Telescope, and capture and server routine

--- seo-capture/Session.py
+++ seo-capture/Session.py
@@ -17,11 +17,9 @@ class Session(object):
     def __init__(self, targets: List[str],
                  exposure_time: float,
                  exposure_count: int = 1,
-                 rgb: bool = True,
+                 filters: List[str] = ["clear"],
                  binning: int = 2,
-                 user: str = "",
-                 close_after: bool = True, 
-                 demo: bool = False):
+                 user: str = ""):
         """ Creates a new imaging session with desired parameters.
 
         Creates a new imaging session that will image each target with exposure_count 
@@ -32,21 +30,13 @@ class Session(object):
                     ['m31, 'C 34', 'NGC 6974']
             exposure_time: the time for each exposure in seconds
             exposure_count: the number of exposures to take for each filter
-            rgb: whether to take images with each of the primary filters
+            filters: a list of strings indicating the desired filters for each exposure
+            binning: the desired CCD binning
             user: the username of the user who requested/created the session
-            close_after: whether the dome should close at the end of the session
-            demo: if this is True, telescope control commands will be
-                    printed to STDOUT and NOT executed. Useful for debugging.
-
         """
-        # The user who created the session
-        if user == "": # take environment variable if not specified
-            self.user = os.environ['USER']
-        else:
-            self.user = user
-            
-        self.__log("Creating new imaging session for user "+str(self.user)+"...",
+        self.__log("Creating new imaging session for user: "+str(self.user)+"...",
                    color="green")
+        
         # A list of strings containing catalog names of objects to be imaged
         # Currently, the objects are imaged in the order they are specified.
         self.targets = targets
@@ -61,19 +51,14 @@ class Session(object):
         self.__log("Exposure Count: "+str(self.exposure_count))
 
         # Whether I, R, G filters should be used 
-        self.rgb = rgb
-        self.__log("Using RGB filters: "+str(self.rgb))
+        self.filters = filters
+        self.__log("Using filters: "+str(self.filters))
 
         # What binning to use
         self.binning = binning
-        self.__log("Binning: "+str(self.binning))
+        self.__log("CCD Binning: "+str(self.binning))
 
-        # Whether to close the dome after the session
-        self.close_after = close_after
-
-        # Whether this is a trial, demo run
-        self.demo = demo
-
+        
     def execute(self) -> int: 
         """ Starts the execution of the imaging session; return's status
         once imaging run is completed. 
@@ -137,196 +122,8 @@ class Session(object):
         return True
 
 
-    def add_target(self, target: Union[str, List[str]]) -> 'Session':
-        """ Adds an additional list of targets to a session. Currently cannot 
-        be called after the session has started executing. 
-
-        N.B. Return type signature is a to-be-processed forward reference
-        to the Session class (as the Session class has yet to be added to
-        the namespace and therefore can't be used as a type hint)
-        """
-        self.targets.append(target)
-        return self
-
-    def set_exposure_time(self, exposure_time: float) -> 'Session':
-        """ Changes the exposure time of the imaging session to a new value
-        """
-        self.exposure_time = exposure_time
-        return self
-
-    def set_exposure_count(self, exposure_count: int) -> 'Session':
-        """ Changes the exposure count of the imaging session to a new value
-        """
-        self.exposure_count = exposure_count
-        return self
-
-    def close(self) -> bool:
-        """ Closes the current session, closes the dome, and logs out. Returns
-        True if successful in closing down, False otherwise.
-        """
-        return self.__run_command("closedown && logout")
-
-    def __del__(self):
-        """ Closes the telescope and logsout of any sessions when the Session
-        is garbage-collected by Python.
-        """
-#        if self.close_after is True:
-#            self.close()
 
             
-    def __log(self, msg: str, color: str = "white") -> bool:
-        """ Prints a log message to STDOUT. Returns True if successful, False
-        otherwise.
-        """
-        return Util.log(msg, color)
 
-    
-    def __run_command(self, command: str) -> str:
-        """ Executes a shell command either locally, or remotely via ssh. 
-        Returns the byte string representing the captured STDOUT
-        """
-        if self.demo == True:
-            self.__log("[DEMO] Executing {}".format(command), color="magenta")
-        else:
-            self.__log("Executing {}".format(command), color="magenta")
-            try:
-                return subprocess.check_output(command)
-            except:
-                self.__log("Failed while executing {}".format(command), color="red")
-                self.__log("Please manually close the dome by running"
-                           " `closedown` and `logout`.", color="red")
-                exit(1)
-    
-    def __weather_ok(self) -> bool:
-        """ Checks whether the sun has set, there is no rain (rain=0) and that 
-        it is less than 40% cloudy. Returns true if the weather is OK to open up, 
-        false otherwise. 
-        """
 
-        # check sun
-        sun = self.__run_command("sun")
-        if float(find_value("alt", sun)) >= -1.0:
-            return False
 
-        # sun is good - check for weather
-        weather = self.__run_command("tx taux")
-
-        # if this cmd failed, return false to be safe
-        if weather == None or weather == "":
-            return False
-        
-        rain = 1 # default to being raining just in case
-        cloud = 1 # default to being cloudy just in case
-        rain = float(find_value("rain", weather)) # find rain=val
-        cloud = float(find_value("cloud", weather)) # find cloud=val
-
-        if rain == 0 and cloud < 0.4:
-            return True
-        else:
-            return False
-    
-    def __open_dome(self) -> bool:
-        """ Checks that the weather is acceptable, and then opens the dome, 
-        if it is not already open, and  also enables tracking. 
-        """
-        # check if dome is already open
-        if self.__dome_status() == True:
-            return True
-
-        # check that weather is OK to open
-        if self.__weather_ok() == True:
-            result = self.__run_command("openup nocloud &&" 
-                                        "keepopen maxtime=20000 slit"
-                                        "&& track on")
-            if result == True: # everything was good
-                return True
-            else: # one of the commands failed
-                return False
-        else:
-            return False
-
-    def __dome_status(self) -> bool:
-        """ Checks whether the slit is open or closed. Returns True if open, 
-        False if closed.
-        """
-        slit = self.__run_command("tx slit")
-        result = find_value("slit", slit)
-        if result == "open":
-            return True
-
-        return False
-
-    def __goto_target(self, target: str) -> bool:
-        """ Points the telescope at the target in question. Returns True if
-        successfully (object was visible), and returns False if unable to set
-        telescope (failure, object not visible).
-        """
-        if self.__target_visible(target) == True:
-            cmd = "catalog "+target+" | dopoint"
-            return self.__run__command(cmd)
-
-        return False
-
-    def __target_visible(self, target: str) -> bool:
-        """ Checks whether a target is visible, and whether it is > 40 degrees
-        in altitude. Returns True if visible and >40, False otherwise
-        """
-        cmd = "catalog "+target+" | altaz"
-        altaz = self.__run_command(cmd).split()
-        if float(find_value("alt", altaz)) >= 40:
-            return True
-
-        return False
-
-    def __current_filter(self) -> str:
-        """ Returns the name of the currently enabled filter, or
-        clear otherwise. 
-        """
-        return self.__run_command("pfilter")
-        
-    def __change_filter(self, name: str) -> bool:
-        """ Changes filter to the new specified filter. Options are: 
-        u, g, r, i, z, clear, h-alpha. Returns True if successful, 
-        False otherwise
-        """
-        if name == "h-alpha":
-            return self.__run_command("pfilter h-alpha")
-        elif name == "clear":
-            return self.__run_command("pfilter clear")
-        else:
-            return self.__run_command("pfilter "+name+"-band")
-
-    def __take_exposure(self, filename: str) -> bool:
-        """ Takes an exposure of length self.exposure_time saves it in the FITS 
-        file with the specified filename. Returns True if imaging
-        was successful, False otherwise. 
-        """
-        cmd = "image time="+self.exposure_time+" bin="+self.binning+" "
-        cmd += "outfile="+filename+".fits"
-        status = self.__run_command(cmd)
-        self.__log("Saved exposure frame to "+filename, color="cyan")
-        return status
-
-    def __take_bias(self, filename: str) -> bool:
-        """ Takes a bias frame and saves it in the FITS file with the specified
-        filename. Returns True if imaging was successful, False otherwise. 
-        """
-        cmd = "image time=0.5 bin="+self.binning+" "
-        cmd += "outfile="+filename+"_bias.fits"
-        status = self.__run_command(cmd)
-        self.__log("Saved bias frame to "+filename, color="cyan")
-        return status
-
-    def __take_dark(self, filename: str) -> bool:
-        """ Takes an dark exposure of length self.exposure_time saves it in the
-        FITS file with the specified filename. Returns True if imaging
-        was successful, False otherwise. 
-        """
-        cmd = "image time="+self.exposure_time+" bin="+self.binning+" dark "
-        cmd += "outfile="+filename+"_dark.fits"
-        status = self.__run_command(cmd)
-        self.__log("Saved dark frame to "+filename, color="cyan")
-        return status
-
-if __name__ == '__main__':
-    s = Session(['m31'], 60, 5, demo=True)
--- seo-capture/Telescope.py
+++ seo-capture/Telescope.py
@@ -12,47 +12,157 @@ class Telescope(object):
 
     
     def open_dome(self) -> bool:
-        pass
+        """ Checks that the weather is acceptable, and then opens the dome, 
+        if it is not already open, and  also enables tracking. 
+        """
+        # check if dome is already open
+        if self.__dome_status() == True:
+            return True
+
+        # check that weather is OK to open
+        if self.__weather_ok() == True:
+            result = self.__run_command("openup nocloud &&" 
+                                        "keepopen maxtime=20000 slit"
+                                        "&& track on")
+            if result == True: # everything was good
+                return True
+            else: # one of the commands failed
+                return False
+        else:
+            return False
 
     
     def close_dome(self) -> bool:
-        pass
+        """ Closes the current session, closes the dome, and logs out. Returns
+        True if successful in closing down, False otherwise.
+        """
+        return self.__run_command("closedown && logout")
 
     
     def weather_ok(self) -> bool:
-        pass
+        """ Checks whether the sun has set, there is no rain (rain=0) and that 
+        it is less than 40% cloudy. Returns true if the weather is OK to open up, 
+        false otherwise. 
+        """
+
+        # check sun
+        sun = self.__run_command("sun")
+        if float(find_value("alt", sun)) >= -1.0:
+            return False
+
+        # sun is good - check for weather
+        weather = self.__run_command("tx taux")
+
+        # if this cmd failed, return false to be safe
+        if weather == None or weather == "":
+            return False
+        
+        rain = 1 # default to being raining just in case
+        cloud = 1 # default to being cloudy just in case
+        rain = float(find_value("rain", weather)) # find rain=val
+        cloud = float(find_value("cloud", weather)) # find cloud=val
+
+        if rain == 0 and cloud < 0.4:
+            return True
+        else:
+            return False
 
     
     def dome_status(self) -> str:
-        pass
+        """ Checks whether the slit is open or closed. Returns True if open, 
+        False if closed.
+        """
+        slit = self.__run_command("tx slit")
+        result = find_value("slit", slit)
+        if result == "open":
+            return True
+
+        return False
 
     
     def goto_target(self, name: str) -> bool:
-        pass
+        # JUST AN IDEA
+        """ Points the telescope at the target in question. Returns True if
+        successfully (object was visible), and returns False if unable to set
+        telescope (failure, object not visible).
+        """
+        if self.__target_visible(target) == True:
+            cmd = "catalog "+target+" | dopoint"
+            return self.__run__command(cmd)
+
+        return False
 
     
-    def target_visible(self, name: str) -> bool: 
-        pass
+    def target_visible(self, name: str) -> bool:
+        # JUST AN IDEA
+        """ Checks whether a target is visible, and whether it is > 40 degrees
+        in altitude. Returns True if visible and >40, False otherwise
+        """
+        cmd = "catalog "+target+" | altaz"
+        altaz = self.__run_command(cmd).split()
+        if float(find_value("alt", altaz)) >= 40:
+            return True
+
+        return False
 
     
     def current_filter(self) -> str:
-        pass
+        """ Returns the name of the currently enabled filter, or
+        clear otherwise. 
+        """
+        return self.__run_command("pfilter")
 
     
     def change_filter(self, name: str) -> str:
-        pass
+        # JUST AN IDEA
+        """ Changes filter to the new specified filter. Options are: 
+        u, g, r, i, z, clear, h-alpha. Returns True if successful, 
+        False otherwise
+        """
+        if name == "h-alpha":
+            return self.__run_command("pfilter h-alpha")
+        elif name == "clear":
+            return self.__run_command("pfilter clear")
+        else:
+            return self.__run_command("pfilter "+name+"-band")
 
     
     def take_exposure(self, filename: str) -> bool:
-        pass
+        # Juat an idea
+        """ Takes an exposure of length self.exposure_time saves it in the FITS 
+        file with the specified filename. Returns True if imaging
+        was successful, False otherwise. 
+        """
+        cmd = "image time="+self.exposure_time+" bin="+self.binning+" "
+        cmd += "outfile="+filename+".fits"
+        status = self.__run_command(cmd)
+        self.__log("Saved exposure frame to "+filename, color="cyan")
+        return status
 
     
     def take_bias(self, filename: str) -> bool:
-        pass
+        # JUST AN IDEA
+        """ Takes a bias frame and saves it in the FITS file with the specified
+        filename. Returns True if imaging was successful, False otherwise. 
+        """
+        cmd = "image time=0.5 bin="+self.binning+" "
+        cmd += "outfile="+filename+"_bias.fits"
+        status = self.__run_command(cmd)
+        self.__log("Saved bias frame to "+filename, color="cyan")
+        return status
 
     
     def take_dark(self, filename: str) -> bool:
-        pass
+        ## AMANDA, THIS IS JUST AN IDEA - WORTH A REWRITE
+        """ Takes an dark exposure of length self.exposure_time saves it in the
+        FITS file with the specified filename. Returns True if imaging
+        was successful, False otherwise. 
+        """
+        cmd = "image time="+self.exposure_time+" bin="+self.binning+" dark "
+        cmd += "outfile="+filename+"_dark.fits"
+        status = self.__run_command(cmd)
+        self.__log("Saved dark frame to "+filename, color="cyan")
+        return status
 
     
     def enable_tracking(self) -> bool:
